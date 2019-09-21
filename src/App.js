8ec106cc12760c409231ad810fbe982761bb7fa2
abtorres <abtorres079@gmail.com>
2019-09-21 14:30:49 -0600

    import CartContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -18,13 +19,15 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={ { products, addItem } }>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" />
-				<Route path="/cart" component={Products}/>
-			</div>
+					{/* Routes */}
+					<Route exact path="/" />
+					<Route path="/cart" component={Products}/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
